524994acdd11d843da9e1852b90db8363cc79156
Nathan Webb <nwrocketman@byui.edu>
2021-05-27 01:23:54 -0600

    Completed the initial build of the website.

---
+++ app.py
@@ -0,0 +1,33 @@
+# Import the needed libraries.
+from flask import Flask, render_template
+from flask_session import Session
+
+# Import the controllers.
+from controllers.shop import shop
+
+# Create the applications.
+app = Flask(__name__)
+
+# Configure the sessions in the web app.
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+app.secret_key = "\xd5$\xa2\xd5\xd8\x06\xab\xa4\xb5\x86\xec\xf1Tn[s"
+Session(app)
+
+# Call the shop blueprint.
+app.register_blueprint(shop)
+
+# The 404 handler.
+@app.errorhandler(404)
+def not_found(e):
+    # Gather the need information.
+    kmarge = {
+        'title': 'Page Not Found'
+    }
+
+    # Render the page and return the 404 code.
+    return render_template('404.html', **kmarge), 404
+
+# Start the application.
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ controllers/shop.py
@@ -0,0 +1,64 @@
+# Import the needed libraries.
+from flask import Blueprint, render_template
+
+# Create the shop blueprint.
+shop = Blueprint('shop', __name__, template_folder='templates')
+
+# GET / aka the homepage.
+@shop.route('/')
+def index():
+    # Gather the needed information.
+    kmarge = {
+        'title': 'Home'
+    }
+
+    # Render the page.
+    return render_template('index.html', **kmarge)
+
+# GET /products
+# The routes gives the list of products.
+@shop.route('/products')
+def products():
+    # Gather the needed information.
+    kmarge = {
+        'title': 'Products'
+    }
+
+    # Render the page.
+    return render_template('products.html', **kmarge)
+
+# GET /order
+# The routes gives the order form.
+@shop.route('/order')
+def order():
+    # Gather the needed information.
+    kmarge = {
+        'title': 'Order'
+    }
+
+    # Render the page.
+    return render_template('order.html', **kmarge)
+
+# GET /contact
+# The route gives the contact form.
+@shop.route('/contact')
+def contact_us():
+    # Gather the needed information.
+    kmarge = {
+        'title': 'Contact Us'
+    }
+
+    # Render the page.
+    return render_template('contact.html', **kmarge)
+
+# GET /about
+# The route give the about information.
+@shop.route('/about')
+def about_us():
+    # Gather the needed information.
+    kmarge = {
+        'title': 'About Us'
+    }
+
+    # Render the page.
+    return render_template('about.html', **kmarge)
